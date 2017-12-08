feee200482f1cf6585d3916d8956b454be56177d
Keith Dechant <keith@metaltoad.com>
2017-12-07 16:43:46 -0800

    Added 3 separate AIs and a basic visualization of the route on a map

--- app/models.py
+++ app/models.py
@@ -19,7 +19,7 @@ class Point:
     where
       highway in ('motorway')
     order by distance
-    limit 1
+    limit {limit}
     """
 
     def __init__(self, lat, lon):
@@ -31,11 +31,50 @@ class Point:
         return str(self.geo.latitude) + ", " + str(self.geo.longitude)
 
     def find_nearest_road(self):
-        sql = self.base_sql.format(**{'lat': self.geo.latitude, "lon": self.geo.longitude})
+        sql = self.base_sql.format(**{'lat': self.geo.latitude, "lon": self.geo.longitude, "limit": 1})
         result = db.engine.execute(sql)
         for row in result:
             self.distance = round(row['distance'],1)
             self.direction = round(row['azimuth'],1)
+
+    def find_weighted_direction(self):
+        """
+        Finds the weighted average of the directions to the nearest two roads. Used by the "weighted" algorithm
+        :return:
+        """
+        sql = self.base_sql.format(**{'lat': self.geo.latitude, "lon": self.geo.longitude, "limit": 2})
+        result = db.engine.execute(sql)
+        distance = []
+        direction = []
+        i = 0
+        for row in result:
+            print(row['distance'])
+            print(row['azimuth'])
+            distance.append(round(row['distance'],1))
+            direction.append(round(row['azimuth'],1))
+            i += 1
+
+        new_dir = (direction[0] * distance[1] + direction[1] * distance[0]) / (distance[0] + distance[1])
+        if abs(direction[0] - direction[1]) < 180:
+            new_dir += 180.0
+        print("new dir = " + str(new_dir))
+        return new_dir
+
+    def is_better_than(self, other_point):
+        """
+        Determines if this point (self) is valid and is in a better position than another point (other_point)
+        :param pt: Another Point object, which will be compared to this one
+        :return: boolean True if this point is valid and farther away than the other point. False otherwise.
+        """
+        if not self.is_in_bounds():
+            print("point " + str(self) + " is out of bounds")
+            return False
+        self.find_nearest_road()
+        if self.distance > other_point.distance:
+            print("new point " + str(self) + " is better than " + str(other_point))
+            return True
+        else:
+            print("new point " + str(self) + " is worse than " + str(other_point))
 
     def is_in_bounds(self):
         """
--- app/views.py
+++ app/views.py
@@ -13,48 +13,70 @@ def index():
     for row in result:
         place = row['name']
 
-    # get started with the first point
-    # point = Point(45.51973, -122.67685)     # MT office
-    # point = Point(45.51063, -122.59734)     # Mount Tabor
-    # point = Point(45.51137, -122.69621)     # SW Portland
-    # point = Point(45.44124, -122.75212)       # far SW Portland
-    point = Point(45.5393, -122.4971)       # outer east side near gresham border - for testing edge detection
-    point.find_nearest_road()
-    points = [point]
+    starting_points = [
+        # Point(45.51973, -122.67685),    # MT office
+        Point(45.475835, -122.685968)    # SW Portland near I-5 curve
+        # Point(45.51063, -122.59734),     # Mount Tabor
+        # Point(45.5393, -122.4971)       # outer east side near gresham border - for testing edge detection
+    ]
 
-    # A distance object we will use for moving the point around
-    # (see https://stackoverflow.com/questions/24427828/calculate-point-based-on-distance-and-direction)
-    mover = geopy.distance.VincentyDistance(feet=500)
+    for starting_point in starting_points:
 
-    # look for points that are farther from the road
-    for i in range(25):
-        # try 5 new points: directly away from the road, and 45/90 deg to either side
-        new_points = [
-            mover.destination(point.geo, bearing=point.direction + 90),
-            mover.destination(point.geo, bearing=point.direction + 135),
-            mover.destination(point.geo, bearing=point.direction + 180),
-            mover.destination(point.geo, bearing=point.direction + 225),
-            mover.destination(point.geo, bearing=point.direction + 270)
-        ]
-        current_distance = point.distance
-        best_point = point
-        for p in new_points:
-            pt = Point(p.latitude, p.longitude)
-            if not pt.is_in_bounds():
-                print("point " + str(pt) + " is out of bounds")
-                continue
-            pt.find_nearest_road()
-            if pt.distance > current_distance:
-                print("new point " + str(pt) + " is better")
-                best_point = pt
-                current_distance = pt.distance
+        # get started with the first point
+        # point = Point(45.51973, -122.67685)     # MT office
+        # point = Point(45.51063, -122.59734)     # Mount Tabor
+        # point = Point(45.51137, -122.69621)     # SW Portland
+        # point = Point(45.44124, -122.75212)       # far SW Portland
+        starting_point.find_nearest_road()
+        points = [starting_point]
 
-        if current_distance == point.distance:
-            print("can't do any better")
-            break
+        # Which searching algorithm to use: 'simple', 'fan', 'weighted'
+        algorithm = 'fan'
 
-        points.append(best_point)
-        point = best_point
+        # A distance object we will use for moving the point around
+        # (see https://stackoverflow.com/questions/24427828/calculate-point-based-on-distance-and-direction)
+        mover = geopy.distance.VincentyDistance(feet=1000)
+
+        # look for points that are farther from the road
+        current = starting_point
+        for i in range(20):
+            best_point = current
+            if algorithm == 'simple':
+                print(current)
+                print("moving in simple direction: " + str(current.direction + 180))
+                new_point = mover.destination(current.geo, bearing=current.direction + 180)
+                best_point = Point(new_point.latitude, new_point.longitude)
+                best_point.find_nearest_road()
+
+            elif algorithm == 'fan':
+                # try 5 new points: directly away from the road, and 45/90 deg to either side
+                new_points = [
+                    mover.destination(current.geo, bearing=current.direction + 90),
+                    mover.destination(current.geo, bearing=current.direction + 135),
+                    mover.destination(current.geo, bearing=current.direction + 180),
+                    mover.destination(current.geo, bearing=current.direction + 225),
+                    mover.destination(current.geo, bearing=current.direction + 270)
+                ]
+                for p in new_points:
+                    pt = Point(p.latitude, p.longitude)
+                    if pt.is_better_than(best_point):
+                        best_point = pt
+
+                if best_point.distance == current.distance:
+                    print("can't do any better")
+                    break
+
+            elif algorithm == 'weighted':
+                new_dir = current.find_weighted_direction()
+                print("moving in weighted direction: " + str(new_dir))
+                new_point = mover.destination(current.geo, bearing=new_dir)
+
+                pt = Point(new_point.latitude, new_point.longitude)
+                if pt.is_better_than(best_point):
+                    best_point = pt
+
+            points.append(best_point)
+            current = best_point
 
     return render_template('index.html',
                            title='Home',
